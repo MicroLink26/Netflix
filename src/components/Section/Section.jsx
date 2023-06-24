dab77dce81463f4acdce8db55db8150025717562
Mike <mic.roche@gmail.com>
2023-06-24 15:33:07 +0200

    Section css added

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,11 +1,14 @@
+import "./Section.css";
 const Section = (props) => {
   const { category, images } = props.category;
   return (
     <section>
       <h2>{category}</h2>
-      {images.map((image) => (
-        <img src={image} />
-      ))}
+      <div>
+        {images.map((image) => (
+          <img src={image} />
+        ))}
+      </div>
     </section>
   );
 };
